ea2ef2f918c5dc9e600ed32908531c9d17c432d8
Draqode <Boomjay008@gmail.com>
2022-09-20 11:28:24 +0100

    feat: added fonts and setup styled-components

---
+++ styles/global.ts
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        outline:0;
+        box-sizing:border-box;
+      }
+      #root{
+        margin:0 auto;
+        height: 100%;
+      }
+    @font-face {
+        font-family: 'Bergman';
+        src: local('/fonts/Bergman.woff2'), local('/fonts/Bergman.woff2'),
+        url("/fonts/Bergman.woff2") format('woff2');
+        font-weight: 700;
+        font-style: normal;
+    }
+    body{
+      margin: 0;
+      height: 100%;
+      width: 100%;
+      /* scroll-behavior: smooth; */
+      font-family: "Bergman", sans-serif;
+      cursor: none;
+      background-color: #E6E6E6;
+    }
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
