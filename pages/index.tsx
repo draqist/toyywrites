1908c964e1dd0fcce89e255b51aa308157f655df
Draqode <Boomjay008@gmail.com>
2022-09-20 16:30:02 +0100

    feat: basic layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,29 @@
-import type { NextPage } from 'next'
-import Navbar from '../components/Navbar'
-import { Body } from '../styles/Body'
+import type { NextPage } from "next";
+import Navbar from "../components/Navbar";
+import { Body, BrandTag } from "../styles/Body";
 
 const Home: NextPage = () => {
   return (
     <Body>
       <Navbar />
-      
-      <div style={{height:'500px', backgroundColor: "green", width:'100%', marginTop:'30px'}}>
 
-      </div>
-      <div style={{ height: '500px', backgroundColor: "", width: '100%', marginTop: '30px', fontSize:'clamp(2.5rem,6vw,2rem)', fontWeight:'900', textAlign:'center', display: "flex", alignItems: "center", flexDirection: "column" }}>
-        <h1>FASHION WITH SAUCE </h1>
-        
-        <div style={{fontSize:'28px', fontFamily: "Tiempos", width: "60%", marginTop: "60px", lineHeight: '2'}}> It all began 15 years ago, from the tight crevices of Lagos, Nigeria. The will to make people express themselves with the confidence that their attires gave them. Putting smiles on faces, making people the talk of the town with trendy and stylish designs from all cultural blends. </div>
-      </div>
+      <div style={{ height: "500px", backgroundColor: "green", width: "100%", marginTop: "30px" }}></div>
+      <BrandTag>
+        <div className="line">
+          <div />
+          <h1>FASHION WITH SAUCE </h1>
+          <div />
+        </div>
+
+        <div style={{ fontSize: "28px", fontFamily: "Tiempos", width: "60%", marginTop: "60px", lineHeight: "2" }}>
+          {" "}
+          It all began 15 years ago in the tight crevices of Lagos, Nigeria. The will to make people express themselves
+          with the confidence that their attires gave them. Putting smiles on faces and making people the talk of town
+          with trendy and stylish designs from all cultural blends.{" "}
+        </div>
+      </BrandTag>
     </Body>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- styles/Body.ts
+++ styles/Body.ts
@@ -3,3 +3,26 @@ import styled from "styled-components";
 export const Body = styled.div`
   padding:0 30px;
 `
+
+export const BrandTag = styled.div`
+  height: 500px; 
+  background-color: "";
+  width: 100%; 
+  margin-top: 40px; 
+  font-size:clamp(1rem,4vw,1.285rem); 
+  text-align:center;
+  display: flex; 
+  align-items: center; 
+  flex-direction: column;
+  .line{
+    display: grid;
+    grid-template-columns: repeat(3, 1fr) ;
+    width: 100%;
+    align-items: center;
+    div{
+      background-color: #00000030;
+      height: 1px;
+      /* column-span: 2; */
+    }
+  }
+`
