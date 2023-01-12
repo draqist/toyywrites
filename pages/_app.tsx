7928c40f592e4dd4e5f24a6d3f2e676180327225
Draq <boomjay008@gmail.com>
2023-01-12 07:47:57 +0100

    feat: trying a few things out

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import "../styles/components/blogcard.scss";
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
-import { motion } from "framer-motion";
 import type { NextPage } from "next";
+import BlogCard from "../components/UI/BlogCard";
 import Navbar from "../components/UI/Navbar";
 import styles from "../styles/pages/body.module.scss";
-import { pathVariants, svgVariants } from "../utils/animations/globals";
 const Home: NextPage = () => {
   const {
     carousel_container,
@@ -17,6 +16,7 @@ const Home: NextPage = () => {
     body,
     project_tagline,
     project_slides,
+    recent_blogs,
   } = styles;
   return (
     <div className={body}>
@@ -35,50 +35,6 @@ const Home: NextPage = () => {
             themselves with the confidence that their attires gave them. Putting smiles on faces and making people the
             talk of town with trendy and stylish designs from all cultural blends.
           </div>
-          <div className={brand_svg}>
-            <div className={brand_link}>
-              <h3> DISCOVER DIIJA </h3>
-            </div>
-            <div className="svg">
-              <motion.svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 210.05 95.82"
-                fill="none"
-                stroke="#000000"
-                variants={svgVariants}
-                initial="initial"
-                whileInView="animate"
-              >
-                <motion.path
-                  stroke="#000000"
-                  d="M33.28,24.27a186.31,186.31,0,0,1,119-11.69c6.43,1.51,13,3.51,17.87,8,6.14,5.65,8.51,14.67,7.25,22.91-1.54,10.09-8,19-16.11,25.16s-17.81,10-27.58,12.91a198.35,198.35,0,0,1-68.8,8c-14.06-.84-28.13-3.21-41.27-8.3C13.53,77.36,3.16,70.73.58,60.21-1.34,52.35,1.69,43.9,6.87,37.69S19.15,27.18,26.35,23.5C57.08,7.78,91.87,1.34,126.37,0"
-                  strokeDashoffset="0px"
-                  strokeDasharray="1px 3px"
-                  variants={pathVariants}
-                  initial="initial"
-                  whileInView="whileInView"
-                ></motion.path>
-                <path
-                  className="absolute"
-                  stroke="#edece7"
-                  strokeWidth="2"
-                  d="M33.28,24.27a186.31,186.31,0,0,1,119-11.69c6.43,1.51,13,3.51,17.87,8,6.14,5.65,8.51,14.67,7.25,22.91-1.54,10.09-8,19-16.11,25.16s-17.81,10-27.58,12.91a198.35,198.35,0,0,1-68.8,8c-14.06-.84-28.13-3.21-41.27-8.3C13.53,77.36,3.16,70.73.58,60.21-1.34,52.35,1.69,43.9,6.87,37.69S19.15,27.18,26.35,23.5C57.08,7.78,91.87,1.34,126.37,0"
-                  pathLength="1"
-                  strokeDashoffset="0px"
-                  strokeDasharray="0px 1px"
-                ></path>
-                <path
-                  className="absolute"
-                  stroke="#000000"
-                  strokeWidth="1"
-                  d="M33.28,24.27a186.31,186.31,0,0,1,119-11.69c6.43,1.51,13,3.51,17.87,8,6.14,5.65,8.51,14.67,7.25,22.91-1.54,10.09-8,19-16.11,25.16s-17.81,10-27.58,12.91a198.35,198.35,0,0,1-68.8,8c-14.06-.84-28.13-3.21-41.27-8.3C13.53,77.36,3.16,70.73.58,60.21-1.34,52.35,1.69,43.9,6.87,37.69S19.15,27.18,26.35,23.5C57.08,7.78,91.87,1.34,126.37,0"
-                  pathLength="1"
-                  strokeDashoffset="0px"
-                  strokeDasharray="0px 1px"
-                ></path>
-              </motion.svg>
-            </div>
-          </div>
         </section>
       </div>
       <section className={projects}>
@@ -88,67 +44,21 @@ const Home: NextPage = () => {
           <div />
         </div>
         <div className={project_slides}>
-          {/* <SliderImage />
-            <SliderImage />
-            <SliderImage /> */}
         </div>
       </section>
       <div className={content_container}>
         <section className={brand_tag}>
           <div className={brand_tagline}>
             <div />
-            <h1>FASHION WITH SAUCE </h1>
+            <h1>Recent blog posts </h1>
             <div />
           </div>
-          <div className={brand_brief}>
+          <div className={recent_blogs}>
             {" "}
-            It all began 15 years ago in the tight crevices of Lagos, Nigeria. The will to make people express
-            themselves with the confidence that their attires gave them. Putting smiles on faces and making people the
-            talk of town with trendy and stylish designs from all cultural blends.
-          </div>
-          <div className={brand_svg}>
-            <div className={brand_link}>
-              <h3> DISCOVER DIIJA </h3>
-            </div>
-            <div className="svg">
-              <motion.svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 210.05 95.82"
-                fill="none"
-                stroke="#000000"
-                variants={svgVariants}
-                initial="initial"
-                whileInView="animate"
-              >
-                <motion.path
-                  stroke="#000000"
-                  d="M33.28,24.27a186.31,186.31,0,0,1,119-11.69c6.43,1.51,13,3.51,17.87,8,6.14,5.65,8.51,14.67,7.25,22.91-1.54,10.09-8,19-16.11,25.16s-17.81,10-27.58,12.91a198.35,198.35,0,0,1-68.8,8c-14.06-.84-28.13-3.21-41.27-8.3C13.53,77.36,3.16,70.73.58,60.21-1.34,52.35,1.69,43.9,6.87,37.69S19.15,27.18,26.35,23.5C57.08,7.78,91.87,1.34,126.37,0"
-                  strokeDashoffset="0px"
-                  strokeDasharray="1px 3px"
-                  variants={pathVariants}
-                  initial="initial"
-                  whileInView="whileInView"
-                ></motion.path>
-                <path
-                  className="absolute"
-                  stroke="#edece7"
-                  strokeWidth="2"
-                  d="M33.28,24.27a186.31,186.31,0,0,1,119-11.69c6.43,1.51,13,3.51,17.87,8,6.14,5.65,8.51,14.67,7.25,22.91-1.54,10.09-8,19-16.11,25.16s-17.81,10-27.58,12.91a198.35,198.35,0,0,1-68.8,8c-14.06-.84-28.13-3.21-41.27-8.3C13.53,77.36,3.16,70.73.58,60.21-1.34,52.35,1.69,43.9,6.87,37.69S19.15,27.18,26.35,23.5C57.08,7.78,91.87,1.34,126.37,0"
-                  pathLength="1"
-                  strokeDashoffset="0px"
-                  strokeDasharray="0px 1px"
-                ></path>
-                <path
-                  className="absolute"
-                  stroke="#000000"
-                  strokeWidth="1"
-                  d="M33.28,24.27a186.31,186.31,0,0,1,119-11.69c6.43,1.51,13,3.51,17.87,8,6.14,5.65,8.51,14.67,7.25,22.91-1.54,10.09-8,19-16.11,25.16s-17.81,10-27.58,12.91a198.35,198.35,0,0,1-68.8,8c-14.06-.84-28.13-3.21-41.27-8.3C13.53,77.36,3.16,70.73.58,60.21-1.34,52.35,1.69,43.9,6.87,37.69S19.15,27.18,26.35,23.5C57.08,7.78,91.87,1.34,126.37,0"
-                  pathLength="1"
-                  strokeDashoffset="0px"
-                  strokeDasharray="0px 1px"
-                ></path>
-              </motion.svg>
-            </div>
+            <BlogCard/>
+            <BlogCard/>
+            <BlogCard/>
+            <BlogCard/>
           </div>
         </section>
       </div>
