ec6bb73f0504eecc7e209728daa39596fd6ae0cc
ABDULLAH ABDULFATAH <Boomjay008@gmail.com>
2023-02-07 20:51:38 +0100

    Removing the global config

--- utils/animations/globals.ts
+++ utils/animations/globals.ts
@@ -1,10 +1,1 @@
-const svgVariants = {
-  initial: { opacity: 0 },
-  animate: { opacity: 1, transition: { duration: 2 } },
-};
-const pathVariants = {
-  initial: { pathLength: 0 },
-  whileInView: { pathLength: 1, transition: { duration: 2, ease: "easeIn" } },
-};
 
-export {svgVariants, pathVariants}
